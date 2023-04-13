6a84007794a078380f3a0da923cf4da86d6e3d5a
STUDIO <med@wp.com>
2023-04-13 17:13:05 +0000

    First commit from Dev

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, OUFI!");
+        System.out.println("Hello, OUFI Jenkins Push!");
+
     }
 }
 
